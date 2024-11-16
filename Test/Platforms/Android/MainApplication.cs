32ddee108ad4c4a18f42a9f0ce85479264ba71a8
Shuvo-V10 <wayneshuvo91@gmail.com>
2024-11-16 17:05:16 +0600

    first commit

---
+++ Test/Platforms/Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace Test
+{
+#if DEBUG
+    [Application(UsesCleartextTraffic = true)]
+#else
+    [Application(UsesCleartextTraffic = true)]
+#endif
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
